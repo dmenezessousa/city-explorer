678ec40162bd17778691ad40947cd796725a8fa4
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-09-29 00:00:58 -0400

    card rows

--- src/components/Main/Movies.js
+++ src/components/Main/Movies.js
@@ -1,33 +1,37 @@
 import React from "react";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Col, Row } from "react-bootstrap";
 
 class Movies extends React.Component {
   render() {
     return (
       <>
         <Container>
-          {this.props.moviesData.length > 0 &&
-            this.props.moviesData.map((movie) => {
-              return (
-                <Card style={{ width: "24rem", marginLeft: "35%" }}>
-                  <Card.Img variant="top" src={movie.image_url} />
-                  <Card.Body>
-                    <Card.Title>{movie.title}</Card.Title>
-                    <Card.Text>
-                      Overview: {movie.overview}
-                      <br />
-                      Average Votes: {movie.average_votes}
-                      <br />
-                      Total Votes: {movie.total_votes}
-                      <br />
-                      Popularity: {movie.popularity}
-                      <br />
-                      Released On: {movie.released_on}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              );
-            })}
+          <Row>
+            {this.props.moviesData.length > 0 &&
+              this.props.moviesData.map((movie) => {
+                return (
+                  <Col>
+                    <Card style={{ width: "24rem", marginLeft: "35%" }}>
+                      <Card.Img variant="top" src={movie.image_url} />
+                      <Card.Body>
+                        <Card.Title>{movie.title}</Card.Title>
+                        <Card.Text>
+                          Overview: {movie.overview}
+                          <br />
+                          Average Votes: {movie.average_votes}
+                          <br />
+                          Total Votes: {movie.total_votes}
+                          <br />
+                          Popularity: {movie.popularity}
+                          <br />
+                          Released On: {movie.released_on}
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+          </Row>
         </Container>
       </>
     );
